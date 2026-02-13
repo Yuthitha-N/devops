f09471f0f29c8608b9b7e1f25b37b40d4d33c592
Yuthitha-N <yuthitha2106@gmail.com>
2026-02-13 23:53:36 +0530

    second

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 7000;
+const PORT = process.env.PORT || 7000;
 
+// Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
-app.listen(PORT, () => {
+// Explicit root route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// IMPORTANT: bind to 0.0.0.0
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
